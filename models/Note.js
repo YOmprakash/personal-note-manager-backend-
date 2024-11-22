22b613e734a1dfffc16b028bcb5651182b2e3668
YOmprakash <omprakashyamavaram@gmail.com>
2024-11-22 23:37:13 +0530

    first commit

---
+++ models/Note.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const noteSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, 'Title is required'],
+    },
+    description: {
+      type: String,
+      required: [true, 'Description is required'],
+    },
+    category: {
+      type: String,
+      enum: ['Work', 'Personal', 'Others'],
+      default: 'Others',
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const Note = mongoose.model('Note', noteSchema);
+module.exports = Note;
